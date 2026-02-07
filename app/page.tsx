0ab83a70b3487970de400f2af7d34153d129ba86
David Villamizar <villamizarandresdavid@gmail.com>
2026-02-07 12:16:11 -0400

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import { Link } from "@heroui/link";
-import { Snippet } from "@heroui/snippet";
 import { Code } from "@heroui/code";
 import { button as buttonStyles } from "@heroui/theme";
 
@@ -45,11 +44,9 @@ export default function Home() {
       </div>
 
       <div className="mt-8">
-        <Snippet hideCopyButton hideSymbol variant="bordered">
           <span>
             Get started by editing <Code color="primary">app/page.tsx</Code>
           </span>
-        </Snippet>
       </div>
     </section>
   );
