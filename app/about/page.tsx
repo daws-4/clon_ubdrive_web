d2cb10b7fcf7060a0b1255b755d0eac0f12db2b7
David Villamizar <villamizarandresdavid@gmail.com>
2026-02-07 13:15:01 -0400

    database

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,12 @@
 import { title } from "@/components/primitives";
-
+import { Counter } from "@/components/counter";
 export default function AboutPage() {
   return (
     <div>
       <h1 className={title()}>About</h1>
+      <div className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+        <Counter />
+      </div>
     </div>
   );
 }
